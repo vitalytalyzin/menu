042afa4574d6319421baafa41b900ac94a7fed88
vitalytalyzin <vitaly.talyzin@gmail.com>
2020-11-30 22:32:30 +0300

    реализована логика задания

--- src/App.js
+++ src/App.js
@@ -1,11 +1,124 @@
-import './App.css';
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
-function App() {
+const navigationLinks = [
+  {
+    href: '/',
+    title: 'Главная',
+  },
+  {
+    href: '/drift',
+    title: 'Дрифт-такси',
+  },
+  {
+    href: '/timeattack',
+    title: 'Time Attack',
+  },
+  {
+    href: '/forza',
+    title: 'Forza Karting',
+  },
+];
+
+function Menu() {
+  const [activeLink, setActiveLink] = useState(null);
+
   return (
-    <div>
-      menu
-    </div>
+    <nav className="menu">
+      {navigationLinks.map((link, idx) => (
+        <Link
+          className={`menu__item ${activeLink === idx ? 'menu__item-active' : ''}`}
+          onClick={() => setActiveLink(idx)}
+          to={link.href}
+        >
+          {link.title}
+        </Link>
+      ))}
+    </nav>
   );
 }
 
-export default App;
+function HomePage() {
+  return (
+    <article className="article">
+      <h1 className="article__title">Гоночн​ое такси</h1>
+      <p className="article__paragraph">
+        Гоночн​ое такси – отличная возможность насладиться скоростью и мастерством гонщика,
+        сидя на месте штурмана, и стать свидетелем настоящего мастерства профессиональных
+        инструкторов Сочи Автодрома, в полной мере ощутив крутые виражи на самой современной
+        гоночной трассе России.
+      </p>
+    </article>
+  )
+}
+
+function DriftPage() {
+  return (
+    <article className="article">
+      <h1 className="article__title">Дрифт-такси</h1>
+      <p className="article__paragraph">
+        Только ​на Сочи Автодроме вас ждет уникальная возможность
+        промчаться по трассе Формулы 1 на максимально возможной
+        скорости в управляемом заносе на легендарной «королеве дрифта» Nissan Silvia!
+      </p>
+      <p className="article__paragraph">
+        Прокатитесь на дрифт-такси с вице-чемпионом Межконтинентального
+        кубка по дрифту под эгидой FIA 2017 года Аркадием Цареградцевым и
+        испытайте на себе, что значат скоростные постановки и эффектные заносы!
+      </p>
+    </article>
+  )
+}
+
+function ForzaPage() {
+  return (
+    <article className="article">
+      <h1 className="article__title">Forza Karting Sochi</h1>
+      <p className="article__paragraph">
+        В Олимпийском парке работает новый открытый развлекательный картинг центр мирового уровня!
+      </p>
+      <p className="article__paragraph">
+        Вас ждут большая, широкая, безопасная трасса и супер современные карты
+        европейского производства c двигателями мощностью 13 л.с.
+        Трасса длинной более 500 метров  оборудована системой хронометража,
+        фиксирующей данные заезда в режиме реального времени.
+        По окончании заезда каждый пилот получает распечатку со своими результатами.
+      </p>
+    </article>
+  )
+}
+
+function TimeAttackPage() {
+  return (
+    <article className="article">
+      <h1 className="article__title">Гонка ​​​​​​Time Attack</h1>
+      <p className="article__paragraph">
+        Гонка Time Attack создана для тех, кто любит ставить рекорды –
+        открытое соревнование на гражданских автомобилях не предполагает
+        контактной борьбы пилотов во время заезда, но позволит в полной мере
+        ощутить скорость, найти идеальную траекторию и реализовать весь
+        потенциал своего автомобиля. Каждый участник проезжает по Сочи Автодрому
+        определённое количество кругов, а в зачёт идёт только лучшее время одного
+        круга из всех заездов. Для того, чтобы попробовать свои силы на лучшей
+        гоночной трассе страны, Вам понадобится исправный легковой автомобиль
+        на летних шинах и шлем.
+      </p>
+    </article>
+  )
+}
+
+export default function App() {
+  return (
+    <Router>
+      <div>
+        <Menu />
+        <div className="page">
+          <Route path="/" exact component={HomePage} />
+          <Route path="/drift" component={DriftPage} />
+          <Route path="/timeattack" component={TimeAttackPage} />
+          <Route path="/forza" component={ForzaPage} />
+        </div>
+      </div>
+    </Router>
+  );
+}
